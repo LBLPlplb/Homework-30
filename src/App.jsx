1b7e65acf68b4e90fdc9e1c6f118cabd9044638b
LBLPlplb <liudmilacheb@mail.ru>
2025-11-25 08:03:04 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css'
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
-import {useState} from "react";
-import {navItems} from "./utils/constants.js";
+import { navItems } from "./utils/constants.js";
+import { PageContext } from "./utils/context.js";
 
 function App() {
     const [page, setPage] = useState(navItems[0]);
 
     return (
-        <div className="container-fluid">
-            <Header changePage={setPage}/>
-            <Main page={page}/>
-            <Footer/>
-        </div>
+        <PageContext value={{ page, setPage }}>
+            <div className="container-fluid">
+                <Header />
+                <Main page={page} />
+                <Footer />
+            </div>
+        </PageContext>
     )
 }
 
-export default App
+export default App;
